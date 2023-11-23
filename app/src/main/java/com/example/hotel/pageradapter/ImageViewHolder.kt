c6b5665c2814aecd80c3785b45bc1212e5d168d7
Igor Chaus <igor_chaus@mail.ru>
2023-11-23 10:48:32 +0200

    Refactored PagerAdapter using Binding

--- app/src/main/java/com/example/hotel/pageradapter/ImageViewHolder.kt
+++ app/src/main/java/com/example/hotel/pageradapter/ImageViewHolder.kt
@@ -1,10 +1,6 @@
 package com.example.hotel.pageradapter
 
-import android.view.View
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.hotel.R
+import com.example.hotel.databinding.PhotoHotelBinding
 
-class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val imageView: ImageView = itemView.findViewById(R.id.iv_photo)
-}
+class ImageViewHolder(val binding: PhotoHotelBinding) : RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/hotel/pageradapter/PagerAdapter.kt
+++ app/src/main/java/com/example/hotel/pageradapter/PagerAdapter.kt
@@ -1,21 +1,23 @@
 package com.example.hotel.pageradapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.hotel.R
+import com.example.hotel.databinding.PhotoHotelBinding
 
 class PagerAdapter(private val images: List<String>) : RecyclerView.Adapter<ImageViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.photo_hotel, parent, false)
-        return ImageViewHolder(view)
-    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ImageViewHolder(
+        PhotoHotelBinding.inflate(
+            LayoutInflater
+                .from(parent.context), parent, false
+        )
+    )
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         val imageUrl = images[position]
-        Glide.with(holder.itemView.context).load(imageUrl).into(holder.imageView)
+        Glide.with(holder.itemView.context).load(imageUrl).into(holder.binding.ivPhoto)
     }
 
     override fun getItemCount(): Int {
