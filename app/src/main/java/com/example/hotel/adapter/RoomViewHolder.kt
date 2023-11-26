8b89bb1687c54f4664feb222d36de548a39fcb4f
Igor Chaus <igor_chaus@mail.ru>
2023-11-26 21:38:05 +0200

    Added Delegate Adapter instead RoomListAdapter

--- app/src/main/java/com/example/hotel/adapter/RoomViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hotel.adapter
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.hotel.databinding.ItemRoomBinding
-
-class RoomViewHolder(val binding: ItemRoomBinding): RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/hotel/view/RoomListScreen.kt
+++ app/src/main/java/com/example/hotel/view/RoomListScreen.kt
@@ -2,18 +2,26 @@ package com.example.hotel.view
 
 import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.elveum.elementadapter.simpleAdapter
 import com.example.hotel.HotelApp
 import com.example.hotel.R
+import com.example.hotel.adapter.PagerAdapter
 import com.example.hotel.databinding.RoomListScreenBinding
-import com.example.hotel.adapter.RoomListAdapter
+import com.example.hotel.databinding.ItemRoomBinding
 import com.example.hotel.model.Room
 import com.example.hotel.viewmodel.RoomListViewModel
 import com.example.hotel.viewmodel.RoomListViewModelFactory
+import com.google.android.material.tabs.TabLayoutMediator
 import javax.inject.Inject
 
 
@@ -50,10 +58,28 @@ class RoomListScreen: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = RoomListAdapter()
-        adapter.itemClickListener = {
-            showRoom(it)
+
+        val adapter = simpleAdapter<Room, ItemRoomBinding> {
+            areItemsSame = { oldRoom, newRoom -> oldRoom.id == newRoom.id }
+            bind { room ->
+                roomName.text = room.name
+                val price = String.format("%,d", room.price).replace(",", " ") +
+                        roomName.context.getString(R.string.rub)
+                roomPrice.text = price
+                pricePer.text = room.price_per
+                val adapter = PagerAdapter(room.image_urls)
+                vpHotel.adapter = adapter
+                TabLayoutMediator(tabLayout, vpHotel) { _, _ ->
+                }.attach()
+                setPeculiaritiesLayout(peculiaritiesLayout, room.peculiarities)
+            }
+            listeners {
+                btChooseRoom.onClick { room ->
+                    showRoom(room)
+                }
+            }
         }
+
         binding.rv.adapter = adapter
 
         viewModel.rooms.observe(viewLifecycleOwner) { rooms ->
@@ -90,6 +116,73 @@ class RoomListScreen: Fragment() {
             .add(R.id.container_activity, RoomScreen.getInstance())
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun setPeculiaritiesLayout(
+        layoutPeculiarities: LinearLayout,
+        peculiarities: List<String>
+    ) {
+        layoutPeculiarities.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                layoutPeculiarities.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                val maxWidth = layoutPeculiarities.width
+
+                var currentLineWidth = 0
+                var currentRowLayout = LinearLayout(layoutPeculiarities.context)
+                currentRowLayout.orientation = LinearLayout.HORIZONTAL
+                layoutPeculiarities.addView(currentRowLayout)
+
+                for (peculiarity in peculiarities) {
+                    val peculiarityTextView = TextView(layoutPeculiarities.context)
+                    peculiarityTextView.text = peculiarity
+                    val backgroundText = ContextCompat.getDrawable(
+                        layoutPeculiarities.context,
+                        R.drawable.rounded_corners_peculiarity
+                    )
+                    peculiarityTextView.background = backgroundText
+                    val colorText = ContextCompat.getColor(
+                        layoutPeculiarities.context,
+                        R.color.grey_600
+                    )
+                    peculiarityTextView.setTextColor(colorText)
+                    peculiarityTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16.toFloat())
+
+                    val densityDpi = layoutPeculiarities.resources.displayMetrics.densityDpi
+                    val leftPaddingDp = densityDpi / 160 * 10
+                    val rightPaddingDp = densityDpi / 160 * 10
+                    val topPaddingDp = densityDpi / 160 * 5
+                    val bottomPaddingDp = densityDpi / 160 * 5
+                    peculiarityTextView.setPadding(
+                        leftPaddingDp,
+                        topPaddingDp,
+                        rightPaddingDp,
+                        bottomPaddingDp
+                    )
+
+                    val layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+
+                    layoutParams.setMargins(8, 8, 8, 8)
+
+                    peculiarityTextView.layoutParams = layoutParams
+                    peculiarityTextView.measure(0, 0)
+
+                    if (currentLineWidth + peculiarityTextView.measuredWidth > maxWidth) {
+                        currentRowLayout = LinearLayout(layoutPeculiarities.context)
+                        currentRowLayout.orientation = LinearLayout.HORIZONTAL
+                        layoutPeculiarities.addView(currentRowLayout)
+                        currentLineWidth = 0
+                    }
+
+                    currentRowLayout.addView(peculiarityTextView)
+                    currentLineWidth += peculiarityTextView.measuredWidth
+                }
+            }
+        })
     }
 
 }
